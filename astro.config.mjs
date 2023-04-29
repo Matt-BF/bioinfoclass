3bbd3d7ad753fef42c12e874cbcce406371830ea
Matt_BF <mbfiamenghi@gmail.com>
2023-04-29 18:37:51 -0300

    fixed content and started blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,4 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), image()],
-  output: "server",
-  adapter: vercel({
-    analytics: true
-  })
 });
