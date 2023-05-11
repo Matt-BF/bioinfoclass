594e5abd5f788a1a101a4888f3743dbf0af92350
Matt_BF <mbfiamenghi@gmail.com>
2023-05-10 21:59:53 -0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image(), react()],
+  site: "https://bioinfoclass.com",
+  integrations: [tailwind(), mdx(), image(), react(), sitemap()],
   adapter: vercel({}),
 });
