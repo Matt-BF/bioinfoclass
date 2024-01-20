035a6dc5115342d304a81f4a75f396eadc75de53
Mateus Fiamenghi <mbfiamenghi@gmail.com>
2024-01-19 23:30:41 -0800

    upgrade to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
@@ -10,6 +9,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bioinfoclass.com",
-  integrations: [tailwind(), mdx(), image(), react(), sitemap()],
+  integrations: [tailwind(), mdx(), react(), sitemap()],
   adapter: vercel({}),
 });
