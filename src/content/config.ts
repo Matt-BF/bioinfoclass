ca05042af1d5ea4d479c9772a10493f9101f9824
Mateus Fiamenghi <mbfiamenghi@gmail.com>
2024-01-20 21:01:31 -0800

    fix blog content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
+
 // 2. Define a schema for each collection you'd like to validate.
 const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()).optional(),
-    heroImage: z.string(),
-    author: z.string(),
-    publishedAt: z.date(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).optional(),
+      heroImage: image(),
+      author: z.string(),
+      publishedAt: z.date(),
+    }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
